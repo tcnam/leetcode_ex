8a5394b395d1ba663de47a389bb5311fe1b679ad
NamTran <namtran@MacBook-Air-cua-NamTran.local>
2025-06-17 06:58:38 +0700

    first commit

---
+++ data/175_Combine_Two_Tables.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from typing import (
+    List
+)
+
+def combine_two_tables(person: pd.DataFrame, address: pd.DataFrame) -> pd.DataFrame:
+    col_list: List[str] = ['firstName', 'lastName', 'city', 'state']
+    result_df: pd.DataFrame = (
+        person
+            .merge(
+                right = address
+                , how = 'left'
+                , left_on = ['personId']
+                , right_on = ['personId']
+                , copy = True
+                )
+            .loc[:, col_list]
+    )
+    return result_df
