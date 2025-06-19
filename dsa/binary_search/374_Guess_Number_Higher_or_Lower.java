aa967188b1201a9cdbb681b08e8c993705002006
NamTran <namtran@MacBook-Air-cua-NamTran.local>
2025-06-19 22:10:46 +0700

    add ex for binary search

---
+++ dsa/binary_search/374_Guess_Number_Higher_or_Lower.java
@@ -0,0 +1,27 @@
+package binary_search;
+class Solution {
+    public int guessNumber(int n) {
+        int left = 1;
+        int right = n;
+        while (left <= right){
+            int mid = left + (right-left)/2;
+            int guessResult = guess(mid);
+
+            if (guessResult == 0){
+                return mid;
+            }
+            else if (guessResult == 1) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public int guess(int mid){
+        // jsut sample
+        return 0;
+    }    
+}
--- dsa/sorting/637_widest_vertical_area_between_two_points_containing_no_points.java
+++ dsa/sorting/637_widest_vertical_area_between_two_points_containing_no_points.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 class Solution {
     public int maxWidthOfVerticalArea(int[][] points) {
-        int[] sortedArr = Arrays.stream(points).mapToInt(point -> point[0]).sorted().toArray();
+        int[] sortedArr = Arrays.stream(points).mapToInt((point) -> point[0]).sorted().toArray();
         int maxWidth = 0;
         for (int i = 1; i < sortedArr.length; i ++){
             int tempWidth = sortedArr[i] - sortedArr[i-1];
