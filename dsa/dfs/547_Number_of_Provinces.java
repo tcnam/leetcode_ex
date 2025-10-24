3839abb50f8870c76d956d8067dbb31ce19437f5
tcnam <56421023+tcnam@users.noreply.github.com>
2025-10-24 20:24:32 +0700

    update recursive solution for dfs exec

--- dsa/dfs/547_Number_of_Provinces.java
+++ dsa/dfs/547_Number_of_Provinces.java
@@ -3,6 +3,11 @@ import java.util.*;
 
 class Solution {
     public int findCircleNum(int[][] isConnected) {
+        // return nonRecursiveSolution(isConnected);
+        return recursiveSolution(isConnected);
+    }
+
+    public int nonRecursiveSolution(int[][] isConnected){
         int result = 0;
         Stack<Integer> stack = new Stack<Integer>();
         boolean[] visited = new boolean[isConnected.length];
@@ -28,5 +33,27 @@ class Solution {
             }
         }
         return result;
+    }
+
+    public int recursiveSolution(int[][] isConnected){
+        int result = 0;
+        int numOfCity = isConnected.length;
+        boolean[] visited = new boolean[numOfCity];
+        for (int i = 0; i < isConnected.length; i++){
+            if (!visited[i]){
+                dfs(i, isConnected, visited);
+                result += 1;
+            }
+        }
+        return result;
+    }
+
+    public void dfs(int cur, int[][] isConnected, boolean[] visited){
+        visited[cur] = true;
+        for (int i = 0; i < isConnected[cur].length; i++){
+            if (!visited[i] && isConnected[cur][i] == 1){
+                dfs(i, isConnected, visited);
+            }
+        }
     }
 }
--- dsa/dfs/841_Keys_and_Rooms.java
+++ dsa/dfs/841_Keys_and_Rooms.java
@@ -3,31 +3,58 @@ import java.util.*;
 
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        boolean[] visited = new boolean[rooms.size()];
+        // return nonRecursiveSolution(rooms);
+        return recursiveSolution(rooms);
+    }
+
+    public boolean nonRecursiveSolution(List<List<Integer>> rooms){
+        int roomsSize = rooms.size();
+        boolean[] visited = new boolean[roomsSize];
         Stack<Integer> stack = new Stack<Integer>();
-              
+        
         for (int i = 0; i < rooms.get(0).size(); i++){
             stack.push(rooms.get(0).get(i));
             visited[0] = true;
         }
-        int countVisited = 1;
 
         while (!stack.isEmpty()){
             int curNode = stack.pop();
             if (!visited[curNode]){
                 visited[curNode] = true;
-                countVisited += 1;
                 for (int i = 0; i < rooms.get(curNode).size(); i++){
                     stack.push(rooms.get(curNode).get(i));
                 }
             }
         }
 
-        if (countVisited == rooms.size()){
-            return true;
+        for (int i = 0; i < roomsSize; i++){
+            if (!visited[i]){
+                return false;
+            }
         }
-        else {
-            return false;
+        return true;
+    }
+
+
+    public boolean recursiveSolution(List<List<Integer>> rooms){
+        int length = rooms.size();
+        boolean[] visited = new boolean[length];
+        dfs(0, rooms, visited);
+        for (int i = 0; i < length; i++){
+            if (!visited[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void dfs(int curIdx, List<List<Integer>> rooms, boolean[] visited){
+        visited[curIdx] = true;
+        for (int i = 0; i < rooms.get(curIdx).size(); i++){
+            int adjIdx = rooms.get(curIdx).get(i);
+            if (!visited[adjIdx]){
+                dfs(adjIdx, rooms, visited);
+            }
         }
     }
 }
