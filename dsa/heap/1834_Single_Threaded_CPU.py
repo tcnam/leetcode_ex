bcc9c2fb86f7a02cf880547591837406a6433dcd
tcnam <tcnam2000@gmail.com>
2026-01-04 16:29:02 +0700

    update dsa exec

--- dsa/heap/1834_Single_Threaded_CPU.py
+++ dsa/heap/1834_Single_Threaded_CPU.py
@@ -1,89 +1,69 @@
-from typing import List, Tuple, Dict
+from typing import List, Tuple
 
 class Solution:
-    def assignTasks(self, servers: List[int], tasks: List[int]) -> List[int]:
-        result: List[int] = []
-        serverPool: ServerPool = ServerPool()
+    def getOrder(self, tasks: List[List[int]]) -> List[int]:
+        for i in range(0, len(tasks), 1):
+            tasks[i].append(i)
 
-        for ind, weight in enumerate(servers):
-            serverPool.availServers.addNode((weight, ind))
-        
+        sortedTasks: List[int] = sorted(tasks, key = Solution.sortByFirstVal, reverse = False)
+
         curTime: int = 0
         taskInd: int = 0
 
-        while taskInd < len(tasks):
-            serverPool.freeServers(curTime)
-            serInfoWrapper:  Tuple[bool, Tuple[int]] = serverPool.popServer(tasks[taskInd], curTime)
-            status: bool = serInfoWrapper[0]
-            serInfo: Tuple[int] = serInfoWrapper[1]
-            if status:
-                result.append(serInfo[1])
+        result: List[int] = []
+        minHeap: MinHeap = MinHeap()
+        
+        while taskInd < len(tasks) or minHeap.nodes:
+            if not minHeap.nodes:
+                curTime = max(curTime, sortedTasks[taskInd][0])
+            
+            while taskInd < len(tasks) and sortedTasks[taskInd][0] <= curTime:
+                minHeap.addTask((sortedTasks[taskInd][1], sortedTasks[taskInd][2]))
                 taskInd += 1
-                curTime += 1
-            else:
-                curTime = max(curTime, serInfo[0])
-
+            
+            procTime, ind = minHeap.popTask()
+            result.append(ind)
+            curTime += procTime
+        
         return result
 
-class ServerPool:
-    def __init__(self):
-        self.availServers: MinHeap = MinHeap()
-        self.ocpServers: MinHeap = MinHeap()
-    
-    def popServer(self, procTime:int, curTime:int) -> Tuple[bool, Tuple[int]]:
-        serInfo: Tuple[int] = self.availServers.popNode()
-        if serInfo:
-            freeTime: int = curTime + procTime
-            serWeight: int = serInfo[0]
-            self.ocpServers.addNode((freeTime, serInfo[0], serInfo[1]))
-            return (True, serInfo)
-        else:
-            minSerInfo: Tuple[int] = self.ocpServers.nodes[0]
-            return (False, minSerInfo)
-    
-    def freeServers(self, curTime):
-        while self.ocpServers.nodes and self.ocpServers.nodes[0][0] <= curTime:
-            freeNode: Tuple[int] = self.ocpServers.popNode()
-            weight: int = freeNode[1]
-            ind: int = freeNode[2]
-            self.availServers.addNode((weight, ind))
+    def sortByFirstVal(x:List[int]) -> int:
+        return x[0], x[2]
+
 
 class MinHeap:
     def __init__(self):
         self.nodes: List[Tuple[int]] = []
     
-    def getLeftChildInd(self, ind:int) -> int:
-        return ind*2 + 1
+    def getLeftChildInd(self, ind: int) -> int:
+        return ind * 2 + 1
     
-    def getRightChildInd(self, ind:int) -> int:
-        return ind*2 + 2
+    def getRightChildInd(self, ind: int) -> int:
+        return ind * 2 + 2
     
-    def getParInd(self, ind:int) -> int:
+    def getParInd(self, ind: int) -> int:
         return (ind-1)//2
     
-    def isValidInd(self, ind:int) -> int:
+    def isValidInd(self, ind:int) -> bool:
         return ind >= 0 and ind < len(self.nodes)
     
-    def swap(self, firstInd:int, secondInd:int):
-        if self.isValidInd(firstInd) and self.isValidInd(secondInd):
-            self.nodes[firstInd], self.nodes[secondInd] = self.nodes[secondInd], self.nodes[firstInd]
+    def swap(self, firstInd: int, secondInd):
+        tmpVal: Tuple[int] = self.nodes[firstInd]
+        self.nodes[firstInd] = self.nodes[secondInd]
+        self.nodes[secondInd] = tmpVal
     
-    def heapifyUp(self, ind:int):
-        if not self.isValidInd(ind):
-            return
-
+    def heaptifyUp(self, ind: int):
         parInd: int = self.getParInd(ind)
         if not self.isValidInd(parInd):
             return
-
-        if self.nodes[ind] < self.nodes[parInd]:
-            self.swap(ind, parInd)
-            self.heapifyUp(parInd)
+        if self.nodes[parInd] > self.nodes[ind]:
+            self.swap(parInd, ind)
+            self.heaptifyUp(parInd)
     
     def heaptifyDown(self, ind:int):
         if not self.isValidInd(ind):
             return
-
+        
         leftChildInd: int = self.getLeftChildInd(ind)
         rightChildInd: int = self.getRightChildInd(ind)
 
@@ -99,14 +79,13 @@ class MinHeap:
             self.swap(ind, smallerChildInd)
             self.heaptifyDown(smallerChildInd)
     
-    def addNode(self, serInfo:Tuple[int]):
-        self.nodes.append(serInfo)
-        self.heapifyUp(len(self.nodes) - 1)
+    def addTask(self, task: Tuple[int]):
+        self.nodes.append(task)
+        self.heaptifyUp(len(self.nodes) - 1)
     
-    def popNode(self) -> Tuple[int]:
-        nodeInfo: Tuple[int] = ()
-        if self.nodes:
-            self.swap(0, len(self.nodes) - 1)
-            nodeInfo: Tuple[int] = self.nodes.pop()
-            self.heaptifyDown(0)
-        return nodeInfo
+    def popTask(self) -> int:
+        self.swap(0, len(self.nodes) - 1)
+        task: Tuple[int] = self.nodes.pop(len(self.nodes) - 1)
+        self.heaptifyDown(0)
+        return task
+s
